219bf9d2cce6f20a7dacb9658a0dde8fbe55d084
Zaqhry <96840072+Zaqhry@users.noreply.github.com>
2022-01-24 20:18:12 -0500

    Update Wins & Win Percentage.sql

--- Wins & Win Percentage.sql
+++ Wins & Win Percentage.sql
@@ -11,18 +11,19 @@
 WITH FXCTE AS
 (
 SELECT Session,
-	   COUNT(OutcomeFTP) TotalWinsFTP,
-	   (SELECT COUNT(OutcomeFTP) FROM AUDJPY) TotalTrades,
-	   ROUND(SUM(TradeID) / (COUNT(OutcomeFTP)),1) WinPercentageFTP
+       COUNT(OutcomeFTP) TotalWinsFTP,
+       (SELECT COUNT(OutcomeFTP) FROM AUDJPY) TotalTrades,
+       ROUND(SUM(TradeID) / (COUNT(OutcomeFTP)),1) WinPercentageFTP
 FROM AUDJPY 
 WHERE OutcomeFTP = 'Win' 
-	GROUP BY Session,OutcomeFTP
+	GROUP BY Session,
+		 OutcomeFTP
 )
-SELECT Session,
-	   (SELECT COUNT(OutcomeFTP) FROM AUDJPY) TotalTrades,
-	   TotalWinsFTP TotalWinsBySession,
-	   (SELECT(SUM(TotalWinsFTP)) FROM FXCTE) TotalWinsCombined,
-	   WinPercentageFTP WinPercentageBySession
+SELECT Session,   
+       (SELECT COUNT(OutcomeFTP) FROM AUDJPY) TotalTrades,
+       TotalWinsFTP TotalWinsBySession,
+       (SELECT(SUM(TotalWinsFTP)) FROM FXCTE) TotalWinsCombined,
+       WinPercentageFTP WinPercentageBySession
 FROM FXCTE;
 
 --Wins Per Session,Total Trades,Total Wins & Win Percetange Per Session For TSL
@@ -30,18 +31,19 @@ FROM FXCTE;
 WITH FXCTE AS
 (
 SELECT Session,
-	   COUNT(OutcomeTSL) TotalWinsTSL,
-	   (SELECT COUNT(OutcomeTSL) FROM AUDJPY) TotalTrades,
-	   ROUND(SUM(TradeID) / (COUNT(OutcomeTSL)),1) WinPercentageTSL
+       COUNT(OutcomeTSL) TotalWinsTSL,   
+       (SELECT COUNT(OutcomeTSL) FROM AUDJPY) TotalTrades,
+       ROUND(SUM(TradeID) / (COUNT(OutcomeTSL)),1) WinPercentageTSL
 FROM AUDJPY 
 WHERE OutcomeTSL = 'Win' 
-	GROUP BY Session,OutcomeTSL
+	GROUP BY Session,
+		 OutcomeTSL
 )
 SELECT Session,
-	   (SELECT COUNT(OutcomeTSL) FROM AUDJPY) TotalTrades,
-	   TotalWinsTSL TotalWinsBySession,
-	   (SELECT(SUM(TotalWinsTSL)) FROM FXCTE) TotalWinsCombined,
-	   WinPercentageTSL WinPercentageBySession
+       (SELECT COUNT(OutcomeTSL) FROM AUDJPY) TotalTrades,   
+       TotalWinsTSL TotalWinsBySession,
+       (SELECT(SUM(TotalWinsTSL)) FROM FXCTE) TotalWinsCombined,
+       WinPercentageTSL WinPercentageBySession
 FROM FXCTE;
 
 --FTP Overall Win Percentage For All Sessions Combined
@@ -60,9 +62,9 @@ WHERE OutcomeTSL = 'Win';
 
 SELECT DISTINCT Confluence,
 	            COUNT(Confluence) Confluences,
-				SUM(ProfitLossFTP) TotalProfit,
-				ROUND(AVG(ProfitLossFTP),0) AvgProfit,
-				(SELECT SUM(ProfitLossFTP) FROM AUDJPY) OverallProfit
+		    SUM(ProfitLossFTP) TotalProfit,
+		    ROUND(AVG(ProfitLossFTP),0) AvgProfit,
+	            (SELECT SUM(ProfitLossFTP) FROM AUDJPY) OverallProfit
 FROM AUDJPY 
 WHERE OutcomeFTP = 'Win'
 	GROUP BY Confluence
@@ -71,11 +73,14 @@ WHERE OutcomeFTP = 'Win'
 --TSL Profit Based On Confluences
 
 SELECT DISTINCT Confluence,
-				COUNT(Confluence) Confluences,
-				SUM(ProfitLossTSL) TotalProfit,
-				ROUND(AVG(ProfitLossTSL),0) AvgProfit,
-				(SELECT ROUND(SUM(ProfitLossTSL),0) FROM AUDJPY) OverallProfit
+	        COUNT(Confluence) Confluences,
+		SUM(ProfitLossTSL) TotalProfit,
+		ROUND(AVG(ProfitLossTSL),0) AvgProfit,
+		(SELECT ROUND(SUM(ProfitLossTSL),0) FROM AUDJPY) OverallProfit
 FROM AUDJPY 
 WHERE OutcomeTSL = 'Win'
 	GROUP BY Confluence
 	ORDER BY 2 DESC
+	
+	
+	
